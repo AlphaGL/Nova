8a7f8921508c062b7355183f2c2e763de237b9ad
Ibeawuchi chukwugozirim <ibeawuchichukwugozirim@gmail.com>
2025-05-31 03:36:12 +0100

    vault backend test4

--- vault/admin.py
+++ vault/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from . models import *
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Video)
+admin.site.register(Favorite)
+admin.site.register(Collection)
+admin.site.register(Suggestion)
